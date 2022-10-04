965d21bbc0681f427b9154c8d6a32ff832fdc9cd
faristi <fedearistimuno@gmail.com>
2022-10-03 21:01:38 -0300

    Agrego Template

--- proyectocoder/AppCoder/models.py
+++ proyectocoder/AppCoder/models.py
@@ -1,3 +1,9 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
+
+class Familiar(models.Model):
+    nombre = models.CharField(max_length=30)
+    edad = models.IntegerField()
+    fecha_ingreso = models.DateField()
--- proyectocoder/AppCoder/views.py
+++ proyectocoder/AppCoder/views.py
@@ -3,6 +3,20 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import render
+
+from AppCoder.models import Familiar
 
 def mostrar_datos(request):
-    return HttpResponse("Mostrando Datos")
+
+    template = loader.get_template("template1.html")
+
+    familiar_1 = Familiar(nombre="Maria", edad="42", fecha_ingreso="2020-02-15")
+    familiar_2 = Familiar(nombre="Juan", edad="10", fecha_ingreso="2022-02-01")
+    familiar_3 = Familiar(nombre="Roberto", edad="25", fecha_ingreso="2010-05-05")
+
+    dict_contexto = {"familiar1": familiar_1, "familiar2": familiar_2, "familiar3": familiar_3}
+    respuesta = template.render(dict_contexto)
+    
+    return HttpResponse(respuesta)
